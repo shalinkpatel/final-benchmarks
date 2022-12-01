2b5dbd1320278538afd7e8b5d5c4f8cd7800642f
Shalin Patel <shalin.kp.patel@gmail.com>
2022-11-30 22:02:48 -0500

    fixed test oops :)

--- benchmarks/inlining-after-const-prop.lisp
+++ benchmarks/inlining-after-const-prop.lisp
@@ -1,3 +1,3 @@
 (define (f x) (add1 x))
 
-(if true (if true (if true (if true (if true (if true (if true (if true (if true (if true (if true (if true (if true (f 1)) (f 1)) (f 1)) (f 1)) (f 1)) (f 1)) (f 1)) (f 1)) (f 1)) (f 1)) (f 1)) (f 1)) (f 1))
+(if true (if true (if true (if true (if true (if true (if true (if true (if true (if true (if true (if true (if true (f 1) 1) (f 1)) (f 1)) (f 1)) (f 1)) (f 1)) (f 1)) (f 1)) (f 1)) (f 1)) (f 1)) (f 1)) (f 1))
